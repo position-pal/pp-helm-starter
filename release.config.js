c0561444dbb3edb1d1afd11d18c137afd544f1ac
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-12-09 17:15:00 +0100

    ci: add semantic release configuration for helm and conventional-commits messages

---
+++ release.config.js
@@ -0,0 +1,15 @@
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    "@semantic-release/github",
+    "@semantic-release/git",
+    [
+        "semantic-release-helm",
+        {   
+            // Any other configuration should be specified here
+            //registry: 'localhost:5000/repo/chart'
+        }
+    ]
+)
+
+module.exports = config
+

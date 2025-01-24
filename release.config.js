b407ff85edb6fb18c642855de834844041d95787
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-01-24 17:18:17 +0100

    build(deps): change semantic release plugin for helm

--- release.config.js
+++ release.config.js
@@ -1,15 +1,14 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
+    [
+        "semantic-release-helm3",
+        {   
+            "chartPath": "."
+        }
+    ],
     "@semantic-release/github",
     "@semantic-release/git",
-    [
-        "@pubgcorp/semantic-release-helm",
-        {   
-            "ociRegistry": "ghcr.io/position-pal",
-            "versionUpdatePolicy": "sync",
-            "appVersionUpdatePolicy": "desync"
-        }
-    ]
+    
 )
 
 module.exports = config

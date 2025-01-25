9033ccecb0bf261e5331ed96a903b8e87dee98b8
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-01-25 17:12:58 +0100

    ci: updating Chart version using exececutable script

--- release.config.js
+++ release.config.js
@@ -1,14 +1,9 @@
-var publishCmd = `
-yq e '.version = "{{nextRelease.version}}"' -i './Chart.yaml'
-yq e '.appVersion = "{{nextRelease.version}}"' -i './Chart.yaml'
-`
-
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
     [
         "@semantic-release/exec",
         {
-            "publishCmd": publishCmd,
+            "prepareCmd": "./update-helm-version.sh ${nextRelease.version}"
         }
     ],
     "@semantic-release/github",
